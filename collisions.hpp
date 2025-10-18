5ca7580e9762abf397fd324a614f24d047ab1d2d
kpowkitty <witchikittikat@gmail.com>
2025-10-17 20:15:03 -0700

    coll & play

--- collisions.hpp
+++ collisions.hpp
@@ -1,7 +1,19 @@
-#pragma once
-
 template<typename T1, typename T2>
-bool intersect(T1 &obj1, T2 &obj2);
+bool intersect(T1 &obj1, T2 &obj2) {
+	return obj1.location() == obj2.location();
+}
 
+// 1 == player
+// 2 == object
 template <typename T1, typename T2>
-bool check_collision(T1 &obj1, T2 &obj2);
+bool check_collision(T1 &obj1, T2 &obj2) {
+	if (obj2.type() == POISON) {
+		obj1.update_health(-1);
+	} else if (obj2.type() == RAIN) {
+		if (obj1.get_coins() >= 10) {
+			obj1.life(1);
+		}
+
+		obj1.update_coins(1);
+	}
+}
